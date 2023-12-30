552c598c5a0301accce210f666d405903ab150da
kushal <kushalbhetuwal@gmail.com>
2023-12-30 11:09:44 +1100

    added all the components and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter } from "react-router-dom"
+import{Navbar,About, Contact, Experience, Feedbacks,Hero,StarsCanvas,Tech,Works} from './components'
 const App = () => {
   return (
-    <div>
-      Kushal Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
