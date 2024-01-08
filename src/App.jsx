a86799387c0aa70425a18138ff76bc553bc09d06
kushal <kushalbhetuwal@gmail.com>
2024-01-09 04:54:32 +1100

    contact section done

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,35 @@ import {
   Tech,
   Works,
 } from "./components";
+import { useEffect, useRef, useState } from "react";
+import sound from "./assets/sound/sound.mp3";
+import soundon from "./assets/sound/soundon.png";
+import soundoff from "./assets/sound/soundoff.png";
 const App = () => {
+  const audioRef = useRef(new Audio(sound));
+  audioRef.current.volume = 0.3;
+  audioRef.current.loop = true;
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    const playAudio = async () => {
+      try {
+        await audioRef.current.play();
+      } catch (err) {
+        console.error("Audio play failed", err);
+        // Handle the failure to play audio here, if needed
+      }
+    };
+
+    if (isPlaying) {
+      playAudio();
+    }
+
+    return () => {
+      audioRef.current.pause();
+    };
+  }, [isPlaying]);
+
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
@@ -25,6 +53,13 @@ const App = () => {
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
+        </div>
+        <div>
+          <img
+            src={!isPlaying ? soundoff : soundon}
+            className="w-10 h-10 fixed bottom-4 right-4 z-50 cursor-pointer"
+            onClick={() => setIsPlaying(!isPlaying)}
+          />
         </div>
       </div>
     </BrowserRouter>
--- src/components/StarsCanvas.jsx
+++ src/components/StarsCanvas.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { PointMaterial, Points, Preload } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import * as random from "maath/random/dist/maath-random.esm";
+import React, { Suspense, useRef, useState } from "react";
+
+const Stars = (props) => {
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x = ref.current.rotation.x + delta / 10;
+    ref.current.rotation.y = ref.current.rotation.y + delta / 15;
+  });
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} strid={3} frustumCulled>
+        <PointMaterial
+          transparent
+          color="#DCF2F1"
+          size={0.004}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
 
 const StarsCanvas = () => {
   return (
-    <div>StarsCanvas</div>
-  )
-}
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
 
-export default StarsCanvas
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
