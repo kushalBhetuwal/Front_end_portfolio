ab21fdd98edc3818d22065aa0f33daba8f50b3d7
kushal <kushalbhetuwal@gmail.com>
2024-01-12 11:47:47 +1100

    modified the work section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Tilt } from 'react-tilt'
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-import { fadeIn, textVariant } from "../utils/motion";
+
 import { styles } from "../styles";
-import { SectionWrapper } from "../Hoc";
-import { projects } from "../constants";
 import { github } from "../assets";
-import StarsCanvas from "./StarsCanvas";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
   index,
@@ -14,14 +14,17 @@ const ProjectCard = ({
   description,
   tags,
   image,
-  source_code_link,
   live_link,
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
-        options={{ max: 25, scale: 1, speed: 250 }}
-        className=" p-4 bg-tertiary rounded-2xl sm:w-[460px] w-full"
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div
           className="relative w-full h-[230px] cursor-pointer "
@@ -44,7 +47,7 @@ const ProjectCard = ({
           />
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
-              onClick={() => window.open(source_code_link, "_blank")}
+              onClick={() => window.open(live_link, "_blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
@@ -79,13 +82,14 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText}`}>My Work</p>
-        <p className={`${styles.sectionHeadText}`}>Projects</p>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
-      <div className="w-full flex flex-wrap">
+
+      <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3  text-secondary text-[17px] max-w-3xl leading-[30px]"
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
@@ -94,7 +98,8 @@ const Works = () => {
           and manage projects effectively.
         </motion.p>
       </div>
-      <div className="mt-20 gap-10  flex flex-wrap justify-between">
+
+      <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
